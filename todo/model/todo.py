edcbe7f821c98ab2668ccbec4ede8500e8318e26
Cristopher <geronimosanchezromero@gmail.com>
2024-08-22 11:47:34 -0500

    point 6

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -23,3 +23,7 @@ class TodoBook:
     def __init__(self):
         self.todos: dict[int, Todo] = {}
 
+    def add_todo(self, title: str, description: str) -> int:
+        code_id: int = len(self.todos) + 1
+        self.todos[code_id] = Todo(code_id, title, description)
+        return code_id
